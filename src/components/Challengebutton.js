afe4f35da806c3980aa6cbb12d299ebc4d76f63e
Dario Milani <runner855@icloud.com>
2021-04-09 10:51:27 +0100

    working on the join challenge card

---
+++ src/components/Challengebutton.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Challengebutton.scss";
+
+const Challengebutton = () => {
+  return (
+    <>
+      <div className="Challengebutton">
+        <img
+          src="https://fitnessmedia.azureedge.net/media/4269/shutterstock_544106977-marathon20runners20192020x201080-compressed.jpg"
+          alt="runner"
+        />
+
+        <div className="Challengebutton_link">
+          <div className="button">Join A Challenge</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Challengebutton;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Challengebutton from "./Challengebutton";
 import Navbar from "./NavBar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
       <div className="homepage_container">
         <h1>Race Challenge Homepage</h1>
       </div>
+
+      <Challengebutton />
     </>
   );
 };
