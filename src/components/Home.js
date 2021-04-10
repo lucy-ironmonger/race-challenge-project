b411e4d59b756801e991e432fc4c37402b0d7684
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2021-04-10 13:49:31 +0100

    completed post request on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,82 +7,23 @@ const USER_LINK = "http://localhost:4001/users";
 let USER_NAME = window.localStorage.username;
 let STRAVA_ID = window.localStorage.stravaId;
 
-//     .then((results) => {
-//       setSavedProperties(results.data);
-//     });
-//   }
-// }, [userID]);
+function sendRequest() {
+  console.log("sending request");
+  return axios({
+    method: "post",
+    url: USER_LINK,
+    data: {
+      username: USER_NAME,
+      stravaID: STRAVA_ID,
+    },
+  })
+    .then((res) => console.log(res))
+    .catch((error) => {
+      throw error;
+    });
+}
 
 const Home = () => {
-  useEffect(() => {
-    console.log(USER_NAME, STRAVA_ID);
-    axios.post(
-      USER_LINK,
-      {},
-      {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: {
-          stravaId: STRAVA_ID,
-          username: USER_NAME,
-        },
-      }
-    );
-  }, []);
-
-  // const handleClick = () => {
-  //   console.log("click before async");
-  //   //   const res = axios.post(USER_LINK, { username: USER_NAME });
-  //   // };
-
-  //   function saveUserToDb() {
-  //     console.log("click after async");
-  //   }
-  // };
-
-  // async function saveUserToDb() {
-  //   const res = await axios.post(
-  //     USER_LINK,
-  //     {},
-  //     {
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: {
-  //         stravaId: window.localStorage.stravaId,
-  //         username: window.localStorage.username,
-  //       },
-  //     }
-  //   );
-  //   console.log("click after async");
-  // }
-
-  // useEffect(() => {
-  //   if (window.localStorage.stravaId) {
-  //     async function saveUserToDb() {
-  //       const headers = {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       };
-
-  //       await axios.post(
-  //         USER_LINK,
-  //         {},
-  //         {
-  //           headers: headers,
-  //           body: {
-  //             stravaId: window.localStorage.stravaId,
-  //             username: window.localStorage.username,
-  //           },
-  //         }
-  //       );
-  //     }
-  //   }
-  // }, []);
-
   return (
     <>
       <Navbar />
