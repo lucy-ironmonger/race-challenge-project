90f8239701b72fa6b2172927519202ab8a3d75a4
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2021-04-10 10:45:16 +0100

    merged dario's changes and working on post request

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,89 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./NavBar";
 import axios from "axios";
 import Challengebutton from "./Challengebutton";
 
-// useEffect(() => {
-//   if (wwindow.localStorage.stravaId) {
-//     axios.post("http://localhost:4001/userchallenge").then((results) => {
+const USER_LINK = "http://localhost:4001/users";
+let USER_NAME = window.localStorage.username;
+
+//     .then((results) => {
 //       setSavedProperties(results.data);
 //     });
 //   }
 // }, [userID]);
 
-// const handleLogout = () => {
-//   window.FB.logout(() => {});
-//   setUserID("");
-// };
-
 const Home = () => {
+  const handleClick = () => {
+    console.log("click before async");
+    //   const res = axios.post(USER_LINK, { username: USER_NAME });
+    // };
+
+    function saveUserToDb() {
+      axios.post(
+        USER_LINK,
+        {},
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: {
+            stravaId: window.localStorage.stravaId,
+            username: window.localStorage.username,
+          },
+        }
+      );
+      console.log("click after async");
+    }
+  };
+  // async function saveUserToDb() {
+  //   const res = await axios.post(
+  //     USER_LINK,
+  //     {},
+  //     {
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: {
+  //         stravaId: window.localStorage.stravaId,
+  //         username: window.localStorage.username,
+  //       },
+  //     }
+  //   );
+  //   console.log("click after async");
+  // }
+
+  // useEffect(() => {
+  //   if (window.localStorage.stravaId) {
+  //     async function saveUserToDb() {
+  //       const headers = {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //       };
+
+  //       await axios.post(
+  //         USER_LINK,
+  //         {},
+  //         {
+  //           headers: headers,
+  //           body: {
+  //             stravaId: window.localStorage.stravaId,
+  //             username: window.localStorage.username,
+  //           },
+  //         }
+  //       );
+  //     }
+  //   }
+  // }, []);
+
   return (
     <>
       <Navbar />
-
       <div className="homepage_container">
         <h2>Hi, {window.localStorage.firstName}</h2>
-        <button>Add athlete to database</button>
+        <button onClick={handleClick}>Add athlete to database</button>
       </div>
-
       <Challengebutton />
     </>
   );
