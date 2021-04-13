a70e3a0055df3e1ef70d9efd2cedaec5efeff52b
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2021-04-13 21:00:12 +0100

    Co-authored-by: Scott Hannabuss <scott-hannabuss@users.noreply.github.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./NavBar";
 import axios from "axios";
 import Challengebutton from "./Challengebutton";
 
-const USER_LINK = "http://localhost:4001/users";
+const USER_DB_LINK = "http://localhost:4001/users";
+const USER_CHALLENGE_DB_LINK = "http://localhost:4001/userchallenge";
 let USER_NAME = window.localStorage.username;
 let STRAVA_ID = window.localStorage.stravaId;
 
 // 201 = user not in db
 // 200 = user already there
 
-function getRequest() {
+/////
+function getRequestUserDb() {
   axios
-    .get(`${USER_LINK}/${STRAVA_ID}`)
+    .get(`${USER_DB_LINK}/${STRAVA_ID}`)
     .then((res) => {
       if (res.status === 200) {
         console.log("No need to send your info, you're already in the DB", res);
       }
       if (res.status === 201) {
         console.log("Ooh hello you're new here. We're adding you to our DB.");
-        postRequest();
+        postRequestUserDb();
       }
     })
     .catch((error) => {
       throw error;
     });
 }
-getRequest();
+getRequestUserDb();
 
-function postRequest() {
+function postRequestUserDb() {
   console.log("Added ya.");
   return axios({
     method: "post",
-    url: USER_LINK,
+    url: USER_DB_LINK,
     data: {
       username: USER_NAME,
       stravaId: STRAVA_ID,
@@ -44,9 +46,32 @@ function postRequest() {
     });
 }
 
-// postRequest();
+/////
+
+// 201 = user not in a challenge
+// 200 = user is in one
 
 const Home = () => {
+  useEffect(() => {
+    function getRequestUserChallengeDb() {
+      axios
+        .get(`${USER_CHALLENGE_DB_LINK}/${STRAVA_ID}`)
+        .then((res) => {
+          if (res.status === 200) {
+            console.log("You're in a challenge", res);
+          }
+          if (res.status === 201) {
+            console.log("Would you like to be in one?");
+            // postRequestUserDb();
+          }
+        })
+        .catch((error) => {
+          throw error;
+        });
+    }
+    getRequestUserChallengeDb();
+  }, []);
+
   return (
     <>
       <Navbar />
